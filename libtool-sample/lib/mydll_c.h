b5bf6d5d148c5f6f37cf86d71dff55f7f10a851a
Hisashi Horikawa (堀川 久) <hisashi.horikawa@gmail.com>
2023-03-12 00:20:55 +0900

    wip.

---
+++ libtool-sample/lib/mydll_c.h
@@ -0,0 +1,12 @@
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int foo;
+
+extern int func(const char* hoge);
+  
+#ifdef __cplusplus
+}
+#endif
---
+++ libtool-sample/test/mydll_test.cpp
@@ -0,0 +1,11 @@
+
+#include "../lib/mydll_c.h"
+#include "../lib/mydll.h"
+
+int main()
+{
+  func(NULL);
+  C c;
+  
+  return 0;
+}
