1490f1ac8ea41037c7b45c56d1bc1c54ce2602f0
Hisashi Horikawa (堀川 久) <hisashi.horikawa@gmail.com>
2024-10-27 22:06:35 +0900

    fix.

--- libtool-samples/dll1/mydll_test.cpp
+++ libtool-samples/dll1/mydll_test.cpp
@@ -7,10 +7,14 @@
 
 int main()
 {
+#ifdef _WIN32
     // wstring_convert は C++17 で非推奨, C++26 で廃止
+    std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> cvt;
+#else
     std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> cvt;
+#endif
     
-    std::u16string d = cvt.from_bytes("日本語の文字列");
+    hoge::UniString d = cvt.from_bytes("日本語の文字列");
     hoge::Fuga* p = new hoge::Hage(d);
     printf("tarai = %d\n", p->f());
     
